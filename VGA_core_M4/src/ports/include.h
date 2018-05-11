f01846b9ff96bdc55e09dfefeb6c01adc1cc2213
FPSUsername <FPSUsername@outlook.com>
2018-05-11 22:28:18 +0200

    Initial commit

---
+++ VGA_core_M4/src/ports/include.h
@@ -0,0 +1,23 @@
+// Debugging
+//#define DEBUG
+
+// Standard
+#include  <stdarg.h>
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <string.h>
+#include  <math.h>
+
+// OS
+#include "misc.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_usart.h"
+
+// Libs
+#include "stddef.h"
+#include "uart.h"
+#include "delay.h"
+#include "lcd.h"
+#include "leds.h"
+#include "ui.h"
