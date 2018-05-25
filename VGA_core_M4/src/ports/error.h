e0ac0e66d93adcb5e884830e0a2ca49126537b56
Koets <jan.pekellaar@gmail.com>
2018-05-25 17:51:25 +0200

    Comments aangepast

--- VGA_core_M4/src/ports/error.h
+++ VGA_core_M4/src/ports/error.h
@@ -1,3 +1,15 @@
+//--------------------------------------------------------------
+// File     : error.h
+// Datum    : 24.05.2018
+// Version  : 0.1
+// Author   : N. Koetsveld
+// mods by	: P. Kavvathas, B. Rabenort, N. Koetsveld
+// CPU      : STM32F4
+// IDE      : Atollic TrueSTUDIO
+// Module   : CMSIS_BOOT, M4_CMSIS_CORE
+// Function : Error codes,
+//--------------------------------------------------------------
+
 /*
 *********************************************************************************************************
 *                                             ERROR CODES
@@ -5,21 +17,12 @@
 */
 #include "stdint.h"
 
-#define OS_ERR_NONE                     0u
-
+#define ERR_UNKNOWN                     0u
 #define ERR_OUT_OF_BOUND				1u
 #define ERR_COLOR						2u
 #define ERR_INPUT_INVALID				3u
 #define ERR_SIZE						4u
 
-/*
-OSMutexPend(MutexHandle, WAIT_FOREVER, &error);
-if(error)
-{
-   	UART_puts("\n\r0:mutexpend error=");
- 	UART_putint(error);
- 	UART_puts("\n\r");
-}
-*/
+
 void pError(uint8_t errnum);
 uint8_t bound(uint16_t x, uint16_t y, uint8_t *perr);
